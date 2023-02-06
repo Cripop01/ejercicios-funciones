589b9df907516358e891c3eefd6dca0c43f60709
Carlos <camilo@email.com>
2023-02-06 10:23:03 +0100

    ejercicios padawan

---
+++ ejercicio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Ejercicio para hacer un producto entre dos enteros
+public class ejercicio {
+    public static int producto(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Introduce un numero entero: ");
+        int a = in.nextInt();
+        System.out.print("Introduce otro numero entero: ");
+        int b = in.nextInt();
+        System.out.println(a +" * " + b + " = " + producto(a, b));
+    }
+}
