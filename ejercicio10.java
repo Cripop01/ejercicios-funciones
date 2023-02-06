e53cee6bc214283323e4f3c5b32a0c3baa063577
Carlos <camilo@email.com>
2023-02-06 10:45:44 +0100

    clase 6

---
+++ ejercicio10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduce la fecha: ");
+        String in_fecha = in.nextLine();
+        
+        
+    }
+}
