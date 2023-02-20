6035ebf65d02fbc925efdcbe8324d3c5d8c94b4c
Cripop01 <cripoponc@alu.edu.gva.es>
2023-02-20 07:31:12 +0100

    Caso practico del banco terminado

--- calculadora.java
+++ calculadora.java
@@ -1,73 +1,127 @@
 import java.util.Scanner;
 
+/* Supuesto practico calculadora- Tema 5 funciones*/
 public class calculadora {
-    
+    // Funcion que devuelve la suma de dos valores Double
     public static Double suma(Double a, Double b) {
         return a + b;
     }
-
+    
+    //Funcion que  devuelve la resta dos valores Double
     public static Double resta(Double a, Double b) {
-        return  a -b;
+        return a - b;
     }
-
-    public static Double producto(Double a, Double b) {
+    
+    //Funcion que devuelve el producto de dos valores Double
+    public static Double prod(Double a, Double b) {
         return a * b;
     }
-
-    public static Double division(Double a, Double b) {
+    
+    //Funcion que devuelve el coeficiente de dos valores Double
+     
+    public static Double coef(Double a, Double b) {
         return a / b;
     }
-
-    public static boolean check(Double n) {
-        return check(n,0.0,1000000.0);
+    
+    //Metodo que muestra el menu principal y recoge la seleccion del usuario
+    public static int mostrarmenu() {
+        System.out.println("1 - Suma");
+        System.out.println("2 - Resta");
+        System.out.println("3 - Multiplicación");
+        System.out.println("4 - División");
+        System.out.println("5 - Área de un rectangulo");
+        System.out.println("6 - Área de un triangulo equilatero");
+        System.out.println("7 - Área de un circulo");
+        System.out.println("8 - Seno, coseno y tangente de x");
+        System.out.println("9 - Salir");
+        int opcion = new Scanner(System.in).nextInt();
+        pedirvalores(opcion);
+        return opcion;
     }
 
-    public static boolean check(Double n, Double min, Double max) {
-        if(n >= min && n<= max) {
+
+    public static boolean comprobar(Double num, Double min, Double max) {
+        if(num <= max && num >= min) {
             return true;
         } else {
             return false;
         }
     }
 
-    public static void Calculo(int opcion, Double a, Double b) {
-        switch(opcion) {
+    public static Double pedirvalor(Double min, Double max) {
+        Double res = null;
+        boolean valido;
+        String texto = "Introduce un valor entre " + min + " y " + max;
+        do {
+            System.out.println(texto);
+            res = pedirvalor(false);
+            valido = comprobar(res, min, max);
+            texto = "Numero incorrecto. Debe ser entre " + min + " y " + max;
+        } while(!valido);
+
+        return res;
+    }
+
+    public static Double pedirvalor(boolean txt) {
+        if(txt) {
+            System.out.println("Introduce un numero: ");
+        }
+        return new Scanner(System.in).nextDouble();
+    }
+
+    public static void pedirvalores(int opcion) {
+        if(opcion == 8) {
+            calculo(opcion, pedirvalor(-360.0 , 360.0), 0.0);
+        } else if(opcion == 9) {
+            calculo(opcion, null, null);
+        } else if(opcion == 7) {
+            calculo(opcion, pedirvalor(0.0, 1000000.0), 0.0);
+        } else if(opcion >= 5) {
+            calculo(opcion, pedirvalor(0.0, 1000000.0), pedirvalor(0.0, 1000000.0));
+        } else {
+            calculo(opcion, pedirvalor(true), pedirvalor(true));
+        }
+    }
+
+
+    public static void calculo(int opcion, Double a, Double b) {
+        switch (opcion) {
             case 1:
-
+                System.out.println(a + " + " + b + " = " + suma(a, b));
+                break;
+            case 2:
+                System.out.println(a + " - " + b + " = " + resta(a, b));
+                break;
+            case 3:
+                System.out.println(a + " * " + b + " = " + prod(a, b));
+                break;
+            case 4:
+                System.out.println(a + "/" + b + " = " + coef(a, b));
+                break;
+            case 5:
+                System.out.println("Area del rectangulo: " + prod(b, a));
+                break;
+            case 6:
+                System.out.println("Area del triangulo equilatero: " + coef(prod(b, a), 2.0));
+                break;
+            case 7:
+                System.out.println("Area del ciruclo: " + prod(Math.PI, Math.pow(a, 2)));
+                break;
+            case 9:
+                System.out.println("Saliste del menu");
+                break;
+            default:
+                System.out.println("Debes de seleccionar una opcion del 1 al 9");
+                break;
         }
+
+
     }
 
-
-    public static void PedirValores(int opcion) {
-        Scanner in = new Scanner(System.in);
-        if(opcion < 8) {
-            System.out.println("Introduce un valor");
-            Calculo(opcion, in.nextDouble(), in.nextDouble());
-            System.out.println("Introduce otro valor");
-            Calculo(opcion, in.nextDouble(), in.nextDouble());
-        } else if(opcion != 9) {
-            System.out.println("Introduce un valor");
-            check(in.nextDouble());
-        }
-    }
-
-
-    public static void mostrarmenu() {
-        int opcion;
-        System.out.println("1. Suma");
-        System.out.println("2. Resta");
-        System.out.println("3. Multiplicacion");
-        System.out.println("4. Division");
-        System.out.println("5. Area de rectangulo");
-        System.out.println("6. Area de un triangulo equilatero");
-        System.out.println("7. Area de un circulo");
-        System.out.println("8. Seno, coseno y tangente de x");
-        System.out.println("9. Salir");
-        opcion = new Scanner(System.in).nextInt();
-
-    }
-  
     public static void main(String[] args) {
-        
+        int opcion;
+        do {
+            opcion = mostrarmenu();
+        } while(opcion != 9);
     }
 }
