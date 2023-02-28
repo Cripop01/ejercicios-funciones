ca2ab5e7ba56ccd7e27d9ee8f7aedb18d1ca4b5d
serpis <camilo@email.com>
2023-02-28 14:56:29 +0100

    ahorcado final terminado

--- ahorcado_final.java
+++ ahorcado_final.java
@@ -14,7 +14,7 @@ public class ahorcado_final {
     static int vidas = 5; //Vidas que tiene el jugador. Incialmente 5.
 
     //Vector que contiene 50 palabras. Desde el cual se escogera una palabra de forma aleatoria
-    static String[] Banco_Palabras = {"Amor", "Felicidad", "Tristeza", "Esperanza", "Libertad", "Justicia", "Sueño", "Realidad", "Pasión", "Coraje", "Creatividad", "Sabiduría", "Conocimiento", "Belleza", "Fortaleza", "Humildad", "Honestidad", "Respeto", "Tolerancia", "Compasión", "Generosidad", "Gratitud", "Paciencia", "Confianza", "Seguridad", "Aventura", "Riesgo", "Curiosidad", "Integridad", "Lealtad", "Ambición", "Éxito", "Fracaso", "Soledad", "Comunidad", "Solidaridad", "Voluntad", "Sacrificio", "Responsabilidad", "Perseverancia", "Esperanza", "Poder", "Conexión", "Empatía", "Inclusión", "Diversidad", "Igualdad", "Equidad", "Transformación", "Renacimiento"};
+    static String[] Banco_Palabras = {"Amor", "Felicidad", "Tristeza", "Esperanza", "Libertad", "Justicia", "Sueño", "Realidad", "Pasion", "Coraje", "Creatividad", "Sabiduria", "Conocimiento", "Belleza", "Fortaleza", "Humildad", "Honestidad", "Respeto", "Tolerancia", "Compasion", "Generosidad", "Gratitud", "Paciencia", "Confianza", "Seguridad", "Aventura", "Riesgo", "Curiosidad", "Integridad", "Lealtad", "Ambicion", "Exito", "Fracaso", "Soledad", "Comunidad", "Solidaridad", "Voluntad", "Sacrificio", "Responsabilidad", "Perseverancia", "Esperanza", "Poder", "Conexion", "Empatia", "Inclusion", "Diversidad", "Igualdad", "Equidad", "Transformacion", "Renacimiento"};
 
     //Vector de caracteres en el cual se almacenan los caracteres de  la palabra escogida
     static char[] palabra = Banco_Palabras[new Random().nextInt(49)].toUpperCase().toCharArray();
@@ -40,7 +40,7 @@ public class ahorcado_final {
     }
 
     /**
-     * Funcion que muestra las letras adivinadas y las letras utilizadas. Mostrando el vector jugando y usadas.
+     * Funcion que muestra las letras adivinadas y las letras utilizadas, llamando a mostrar_array.
      * @param txt Texto que se va a mostrar al mostrar el tablero de juego
      */
     public static void mostrar(String txt) {
@@ -59,21 +59,21 @@ public class ahorcado_final {
      */
     public static void resultado(char letra) {
         Pattern l = Pattern.compile("[A-Za-z]");
-        if(l.matcher(letra + "").matches()) {
-            if(comprobar(letra, usadas)) {
+        if(l.matcher(letra + "").matches()) { //Si es de la A a la Z
+            if(comprobar(letra, usadas)) { //Si la letra ya ha sido utilizada
                 mostrar("LA LETRA INDICADA YA HA SIDO UTILIZADA");
-            } else {
-                if(comprobar(letra, palabra)) {
+            } else { //Si la letra no ha sido utilizada
+                if(comprobar(letra, palabra)) { //Si la letra esta en la palabra
                     mostrar("HAS ACERTADO");
-                } else {
+                } else { //Si la letra no esta en la palabra
                     mostrar("HAS FALLADO");
                     vidas--;
                 }
-            }
-            if(!comprobar(letra, usadas)) {
+                //Continua, si la letra no ha sido utilizada utilizando la letra al vector usadas
                 usadas[letras_usadas] = letra;
                 letras_usadas++;
             }
+            //Si la letra no es de la A a la Z
         } else {
             mostrar("DEBES USAR UNA LETRA DE LA A a LA Z");
         }
@@ -87,10 +87,17 @@ public class ahorcado_final {
         resultado(in.nextLine().toUpperCase().charAt(0));
     }
 
+    /**
+     * Metood que comprueba si una letra se encuentra en un vector de caracteres
+     * @param letra //Letra que se busca en el vector
+     * @param array //Vector en el que se busca la letra
+     * @return Devuelve un booleano verdadero si encuentra la letra o un booleano falso si no 
+     */
     public static boolean comprobar(char letra, char[] array) {
         boolean res = false;
         for(int i = 0; i<array.length; i++) {
             if(letra == array[i]) {
+                //Si encuentra la letra devuelve true y añade la letra a jugando
                 res = true;
                 jugando[i] = letra;
             }
@@ -98,6 +105,11 @@ public class ahorcado_final {
         return res;
     }
 
+    /**
+     * Metodo que comprueba si palabra es igual a jugando de este modo determina si el jugador
+     * a ganaod la partida
+     * @return Devuelve un booleano que indica si el jugador a ganado o no 
+     */
     public static boolean ganador() {
         if(Arrays.toString(palabra).equals(Arrays.toString(jugando))) {
             return true;
@@ -106,15 +118,19 @@ public class ahorcado_final {
         }
     }
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
+        System.out.println(Arrays.toString(palabra));
         Arrays.fill(jugando, '-');
         while(vidas > 0 && !ganador()) {
             pideletra();
         }
 
+        //Si el jugador a ganado llama a  mostrar con el texto has ganado
         if(ganador()) {
             mostrar("HAS  GANADO");
         } else {
+            //Si el jugador ha perdido llama a mostrar con has perdido y llama a mostrar array
+            //con palabra para mostrar la palbra correcta
             mostrar("HAS PERDIDO");
             mostrar_array("LA PALABRA CORRECTA ES", palabra);
         }
